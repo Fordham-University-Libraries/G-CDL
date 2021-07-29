821fb85ba4b5f1a58adce226f16d83b52f2ca5d9
Witt Meesangnil <wit.m@icloud.com>
2021-07-29 12:12:47 -0400

    Update appsscript.js

--- utils/appsscript.js
+++ utils/appsscript.js
@@ -1,9 +1,10 @@
+const gcdlBaseUrl = 'https://www.library.fordham.edu/digitalreserves'; 
 let config;
-let language;
 
 function main() {
   var sheet = SpreadsheetApp.getActive().getSheets()[0];
-  var values = sheet.getRange('A2:D').getValues();
+  //borrow item, file id, due time, user email, item's library
+  var values = sheet.getRange('A2:E').getValues();
   //Logger.log(values);
   const now = new Date();
   for(let i = 0; i < values.length; i++) {
@@ -11,19 +12,26 @@ function main() {
     let due = new Date(values[i][2] * 1000);
     if (due < now) {
       if (!config) getConfig();
-      if (!language) getLanguage();
       Logger.log('ovedue');
       let id = values[i][1];
       let borrowerEmail = values[i][3];
+      let itemLibrary = values[i][4];
+      
       //return
       var file = DriveApp.getFileById(id);
+      var title = file.getDescription();
       var result = file.removeViewer(borrowerEmail);
-      //remove row fomr sheet
+      
+      //remove row from sheet
       sheet.deleteRow(i + 1); //starting at 1 for the first row
+      
       //email if enabled
-      let subject = "auto returned!";
-      let message = 'yay!';
-      if (config.notifyOnAutoReturn) MailApp.sendEmail(borrowerEmail, subject, message);
+      if (config.notifyOnAutoReturn) {
+        let subject = config.libraries[itemLibrary].lang.returnSubject;
+        let message = config.libraries[itemLibrary].lang.returnBody;
+        Logger.log(subject + ' : ' + message);
+        MailApp.sendEmail(borrowerEmail, subject, message);
+      }
     } else {
       Logger.log('not yet');
     }
@@ -32,11 +40,7 @@ function main() {
 }
 
 function getConfig() {
-  var response = UrlFetchApp.fetch("http://www.library.fordham.edu/digitalreserves/api/?action=get_config");
+  //Logger.log(`${gcdlBaseUrl}/api/?action=get_appsScript_config`);
+  var response = UrlFetchApp.fetch(`${gcdlBaseUrl}/api/?action=get_appsScript_config`);
   config = JSON.parse(response)['data'];
-}
-
-function getLanguage() {
-  var response = UrlFetchApp.fetch("http://www.library.fordham.edu/digitalreserves/api/?action=get_lang");
-  language = JSON.parse(response)['data'];
 }
