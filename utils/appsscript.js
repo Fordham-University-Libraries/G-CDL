5a418746e8411d38844760c33a7d85e2a062fc2c
Witt Meesangnil <wit.m@icloud.com>
2021-07-29 13:36:33 -0400

    Update appsscript.js

--- utils/appsscript.js
+++ utils/appsscript.js
@@ -1,4 +1,21 @@
-const gcdlBaseUrl = 'https://www.library.fordham.edu/digitalreserves'; 
+// use Google Appscript instead of Cron
+// go to Gsheet called "items currently checked out"
+// click Tools -> Script toEditorSettings
+// the in the default file 'Code.gs', copy code below and paste it in (remove all default sample code)
+// update the first line const gcdlBaseUrl = 'https://www.myuniv.edu/G-CDL'; to your actual G-CDL app
+// save
+// click Run, authorize it, make sure no error
+// click on 'Triggers (alarm clock icon) on left bar 
+// click + Add trigger
+// choose (run every minute)
+//   - main
+//   - head
+//   - time-driven
+//   - minutes timer
+//   - every minute
+// save
+
+const gcdlBaseUrl = 'https://www.myuniv.edu/G-CDL';
 let config;
 
 function main() {
@@ -7,40 +24,47 @@ function main() {
   var values = sheet.getRange('A2:E').getValues();
   //Logger.log(values);
   const now = new Date();
-  for(let i = 0; i < values.length; i++) {
+  for (let i = 0; i < values.length; i++) {
     //Logger.log(values[i]);
-    let due = new Date(values[i][2] * 1000);
-    if (due < now) {
-      if (!config) getConfig();
-      Logger.log('ovedue');
-      let id = values[i][1];
-      let borrowerEmail = values[i][3];
-      let itemLibrary = values[i][4];
-      
-      //return
-      var file = DriveApp.getFileById(id);
-      var title = file.getDescription();
-      var result = file.removeViewer(borrowerEmail);
-      
-      //remove row from sheet
-      sheet.deleteRow(i + 1); //starting at 1 for the first row
-      
-      //email if enabled
-      if (config.notifyOnAutoReturn) {
-        let subject = config.libraries[itemLibrary].lang.returnSubject;
-        let message = config.libraries[itemLibrary].lang.returnBody;
-        Logger.log(subject + ' : ' + message);
-        MailApp.sendEmail(borrowerEmail, subject, message);
+    let dueTimestamp = values[i][2];
+    if (dueTimestamp) {
+      let due = new Date(dueTimestamp * 1000);
+      if (due < now) {
+        if (!config) getConfig();
+        Logger.log('ovedue');
+        let id = values[i][1];
+        let borrowerEmail = values[i][3];
+        let itemLibrary = values[i][4];
+
+        //return
+        var file = DriveApp.getFileById(id);
+        var result = file.removeViewer(borrowerEmail);
+
+        //remove row from sheet
+        sheet.deleteRow(i + 1 + 1); //starting at 1 for the first row
+
+        //email if enabled
+        if (config.notifyOnAutoReturn) {
+          let subject = config.libraries[itemLibrary].lang.returnSubject;
+          let title = file.getDescription();
+          let htmlBody = config.libraries[itemLibrary].lang.returnBody.replace('{{$title}}', title);
+          Logger.log(subject + ' : ' + htmlBody);
+          MailApp.sendEmail({
+            to: borrowerEmail,
+            subject: subject,
+            htmlBody: htmlBody,
+          });
+        }
+      } else {
+        Logger.log('not yet');
       }
-    } else {
-      Logger.log('not yet');
     }
   }
 
 }
 
 function getConfig() {
-  //Logger.log(`${gcdlBaseUrl}/api/?action=get_appsScript_config`);
+  Logger.log(`${gcdlBaseUrl}/api/?action=get_appsScript_config`);
   var response = UrlFetchApp.fetch(`${gcdlBaseUrl}/api/?action=get_appsScript_config`);
   config = JSON.parse(response)['data'];
 }
